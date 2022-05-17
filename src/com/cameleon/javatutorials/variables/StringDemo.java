ee8e03bf35246d176d5a1f7300b44306c0665915
jhkang <jhkang1517@gmail.com>
2022-05-17 22:55:20 +0900

    Add variable lecture codes
    Signed-off-by: jhkang <jhkang1517@gmail.com>

---
+++ src/com/cameleon/javatutorials/variables/StringDemo.java
@@ -0,0 +1,11 @@
+package com.cameleon.javatutorials.variables;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String a, b;
+        a = "coding";
+        b = "everybody";
+
+        System.out.println(a + b);
+    }
+}
